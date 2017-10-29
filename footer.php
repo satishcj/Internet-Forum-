30e697a7cd38933c0c3386f743319c7fa6a8532e
17MCS0011 <32630193+17MCS0011@users.noreply.github.com>
2017-10-29 07:17:50 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<?php 
+if(isset($_SESSION['msg'])) { unset($_SESSION['msg']); } ?>
+ <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,13 @@
+<?php 
+error_reporting(0);
+session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>PHP Auth</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+</head>
+<body>
